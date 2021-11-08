d8fe383870651ab4101cce63960f0970fedfd8d9
Andres-GP <andresgarciapedreros@gmail.com>
2021-11-08 20:03:24 +0000

    cookies setting started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,28 @@ import ChannelContainer from "./components/ChannelContainer"
 import ChannelListContainer from "./components/ChannelListContainer"
 import ChannelSearch from "./components/ChannelSearch";
 import Auth from "./components/Auth"
+
 import "./App.css"
+
+const cookies = new Cookies();
 
 const apiKey = 'hnavqk4dpab8';
 
 const client = StreamChat.getInstance(apiKey)
 
-const authToken = false;
+const authToken = cookies.get("token");
+
+if(authToken) {
+    client.connectUset({
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+        cookies.set('phoneNumber', phoneNumber);
+        cookies.set('avatarURL', avatarURL);
+        cookies.set('hashedPassword', hashedPassword);
+    })
+}
 
 const App = () => {
     if(!authToken) return <Auth />
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 
 import signInImage from '../assets/signup.jpg'
 
+
+const cookies = new Cookies();
+
 const initialState = {
     fullName: '',
     userName: '',
@@ -21,12 +24,29 @@ const Auth = () => {
         setForm({...form, [e.target.name]: e.target.value});
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         const { fullName, userName, password, phoneNumber, avatarURL} = form;
 
-        const URL = 'http://localhost:5000';
+        const URL = 'http://localhost:5000/auth';
+
+        const { data: { token, userId, hashedPassword } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName, phoneNumber, avatarURL,
+        });
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+        window.location.reload();
     }
 
     const switchMode = () => {
