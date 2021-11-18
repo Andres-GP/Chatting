c4d7845a2dea3466d030517c68d781c103caa7f7
Andres-GP <andresgarciapedreros@gmail.com>
2021-11-18 19:06:04 +0000

    user list mesage implemente, most of the logic of UserLst.jsx done

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import {AddChannel} from '../assets';
 
-const TeamChannelList = ({children, error= false, loading, type}) => {
+const TeamChannelList = ({children, error= false, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) => {
     if(error) {
         return type === 'team' ? (
             <div className="team-channel-list">
@@ -28,9 +28,16 @@ const TeamChannelList = ({children, error= false, loading, type}) => {
             <div className="team-channel-list__header">
                 <p className="team-channel-list__header__title">
                     { type === 'team' ? 'Channels' : 'Direct Messages'}
-                    {/*Button - add channel */}
                 </p>
+                <AddChannel 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}
+                    type={type === 'team' ? 'team' : 'messaging'}
+                />
             </div>
+            {children}
         </div>
     )
 }
